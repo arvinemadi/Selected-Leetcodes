4617bd448778c740e1d6eb52c050fcaea773c708
Arvin Emadi <arvin.emadi@gmail.com>
2022-06-27 05:24:56 -0500

    Add files via upload

---
+++ Leetcode_630_Course Schedule III_Hard.cpp
@@ -0,0 +1,51 @@
+/*
+There are n different online courses numbered from 1 to n. You are given an array courses where courses[i] = [durationi, lastDayi] indicate that the ith course should be taken continuously for durationi days and must be finished before or on lastDayi.
+
+You will start on the 1st day and you cannot take two or more courses simultaneously.
+
+Return the maximum number of courses that you can take.
+
+ 
+
+Example 1:
+
+Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+Output: 3
+Explanation: 
+There are totally 4 courses, but you can take 3 courses at most:
+First, take the 1st course, it costs 100 days so you will finish it on the 100th day, and ready to take the next course on the 101st day.
+Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day. 
+Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day. 
+The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the closed date.
+Example 2:
+
+Input: courses = [[1,2]]
+Output: 1
+Example 3:
+
+Input: courses = [[3,2],[4,3]]
+Output: 0
+*/
+
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), 
+             [](auto&a, auto&b) 
+             {
+                 return a[1] < b[1];
+             });
+        int sum = 0;
+        priority_queue<int> pq;
+        for(int i = 0; i < courses.size(); i++)
+        {
+            pq.push(courses[i][0]);
+            sum += courses[i][0];
+            if (sum > courses[i][1]) {
+                sum -= pq.top() ;
+                pq.pop();
+            }
+        }
+        return pq.size();
+    }
+};
