b16e1554a0c984ed3dd5f87cd579e704701935c7
Arvin Emadi <arvin.emadi@gmail.com>
2022-06-27 05:25:54 -0500

    Update Leetcode_630_Course Schedule III_Hard.cpp

--- Leetcode_630_Course Schedule III_Hard.cpp
+++ Leetcode_630_Course Schedule III_Hard.cpp
@@ -1,14 +1,11 @@
 /*
-There are n different online courses numbered from 1 to n. You are given an array courses where courses[i] = [durationi, lastDayi] indicate that the ith course should be taken continuously for durationi days and must be finished before or on lastDayi.
+There are n different online courses numbered from 1 to n. You are given an array courses where courses[i] = [durationi, lastDayi] indicate that the ith course 
+should be taken continuously for durationi days and must be finished before or on lastDayi.
 
 You will start on the 1st day and you cannot take two or more courses simultaneously.
-
 Return the maximum number of courses that you can take.
 
- 
-
 Example 1:
-
 Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
 Output: 3
 Explanation: 
@@ -17,10 +14,11 @@ First, take the 1st course, it costs 100 days so you will finish it on the 100th
 Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day. 
 Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day. 
 The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the closed date.
-Example 2:
 
+Example 2:
 Input: courses = [[1,2]]
 Output: 1
+
 Example 3:
 
 Input: courses = [[3,2],[4,3]]
